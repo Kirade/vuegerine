ee851d9e58ca5cc9eb2f9f9c4e7c1e04abc259e8
JaeyoungHeo <jay.jaeyoung@gmail.com>
2019-04-13 15:54:24 +0900

    CHG: comment webpack loader wetting

--- vuegerine/settings.py
+++ vuegerine/settings.py
@@ -118,17 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'vuejs/dist/static'),
 ]
 
 # Django webpack loader 설정
-
-WEBPACK_LOADER = {
-    'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'static/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'vuejs/webpack-stats.json'),
-    },
-}
+#
+# WEBPACK_LOADER = {
+#     'DEFAULT': {
+#         'BUNDLE_DIR_NAME': 'static/',
+#         'STATS_FILE': os.path.join(BASE_DIR, 'vuejs/webpack-stats.json'),
+#     },
+# }
