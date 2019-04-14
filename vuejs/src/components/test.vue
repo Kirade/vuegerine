e500ab234ac868f089cf3d606fbf0207beebb548
JaeyoungHeo <jay.jaeyoung@gmail.com>
2019-04-14 15:16:18 +0900

    ADD: test vue component

---
+++ vuejs/src/components/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+    Hello Test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test'
+}
+</script>
+
+<style scoped>
+.test {
+  text-align: center;
+}
+</style>
