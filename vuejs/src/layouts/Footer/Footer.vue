8ec4206a4b15081eaa0042d235a1c0ce684f4191
JaeyoungHeo <jay.jaeyoung@gmail.com>
2019-04-20 22:21:02 +0900

    ADD: Footer component

---
+++ vuejs/src/layouts/Footer/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="footer-main">
+    Footer Section
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+.footer-main {
+  height: 60px;
+  border: 2px solid;
+}
+</style>
--- vuejs/src/layouts/Layout.vue
+++ vuejs/src/layouts/Layout.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="layout-main">
     <Header></Header>
+    <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from './Header/Header'
+import Footer from './Footer/Footer'
 export default {
   name: 'Layout',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
